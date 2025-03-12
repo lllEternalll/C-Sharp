987ccaa0dc1a2c6d4add145319b22e614108652a
DESKTOP-W11\Eternal <lawermedic.66@gmail.com>
2025-03-12 09:29:49 +0200

    no message

---
+++ ConsoleApp6/ConsoleApp6/Product.cs
@@ -0,0 +1,85 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleApp6
+{
+    public class Product
+    {
+        private string _name;
+        private decimal _price;
+        private int _quantity;
+
+        public Product(string name, decimal price, int quantity)
+        {
+            _name = name;
+            _price = price;
+            _quantity = quantity;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    throw new ArgumentException("Name Error. null or empty");
+                }
+                _name = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price < 0)
+                {
+                    throw new ArgumentException("Price Error. price < 0");
+                }
+                _price = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+        }
+        public decimal TotalValue => _price * _quantity;
+
+
+        public void Restock(int amount)
+        {
+            if (amount > 0)
+            {
+                _quantity += amount;
+            }
+            else
+            {
+                Console.WriteLine("Restock Error. amount <= 0");
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Sell Error. amount <= 0");
+            }
+            if (amount <= _quantity)
+            {
+                _quantity -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Sell Error. amount > quantity");
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"Товар: {_name}, Ціна: {_price}, Кількість: {_quantity}, Загальна вартість: {TotalValue}";
+        }
+    }
+
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Product apple = new Product("Apple", 5, 100);
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Sell(20);
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Restock(50);
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Price = 7;
+            Console.WriteLine(apple.GetInfo());
+
+            apple.Name = "Green Apple";
+            Console.WriteLine(apple.GetInfo());
+
+            try
+            {
+                apple.Price = -10;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                apple.Name = "";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            apple.Sell(200);
+        }
+    }
+}
